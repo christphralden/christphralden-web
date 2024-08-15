4b16c665d7699ac3b489410924a2dc751e027eed
christopher-alden <christopheralden04@gmail.com>
2024-08-15 13:34:28 +0700

    feat: dependencies

---
+++ src/lib/routes/navbar-routes.ts
@@ -0,0 +1,14 @@
+export const NavbarRoutes = [
+    {
+        name:"Home",
+        route:["/"]
+    },
+    {
+        name:"projects",
+        route:["/project"]
+    },
+    {
+        name:"experience",
+        route:["/experience"]
+    },
+]
