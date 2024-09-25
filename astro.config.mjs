8f81e8c54c2fdaa96b0059cee0c5ecddb55a5045
christopher-alden <christopheralden04@gmail.com>
2024-09-25 22:38:39 +0700

    feat: seo and vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
 
+  output: "static",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   vite: {
     ssr: {
-      noExternal: ['gsap'], 
+      noExternal: ["gsap"],
     },
   },
 });
