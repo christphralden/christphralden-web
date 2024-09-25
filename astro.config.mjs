875ee4ba192764b635b57f8295eeb8395b503faf
christopher-alden <christopheralden04@gmail.com>
2024-09-25 22:45:17 +0700

    fix: vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
+import vercelStatic from "@astrojs/vercel/static";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
 
   output: "static",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
   }),
   vite: {
     ssr: {
