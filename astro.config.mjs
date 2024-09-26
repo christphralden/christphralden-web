028fd1ff082cf1607a23dfcf2d2196e4bb9e0875
christopher-alden <christopheralden04@gmail.com>
2024-09-26 09:50:57 +0700

    feat: sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercelStatic from "@astrojs/vercel/static";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
-
+  integrations: [tailwind(), react(), sitemap()],
+  site: "https://christopheralden.dev",
   output: "static",
   adapter: vercelStatic({
     webAnalytics: {
