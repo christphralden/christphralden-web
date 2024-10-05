3d1f034ad1de9d3f1de5eb42b33d28199c689488
christopher-alden <christopheralden04@gmail.com>
2024-10-05 12:44:23 +0700

    feat: layout changes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import { useCallback, useRef, useState, useEffect } from 'react';
 import { NavbarRoutes } from '@lib/routes';
 import { textGlitch } from '@lib/animations/text-effect';
 import { AtSign } from '@geist-ui/icons';
+import Separator from '@components/ui/separator.astro';
 
 export const Navbar = () => {
     const routeRefs = useRef<HTMLParagraphElement[]>([]);
@@ -35,19 +36,19 @@ export const Navbar = () => {
     }, []);
 
     return (
-        <nav className="fixed w-full h-16 md:h-20 flex justify-center px-4 md:px-8 xl:px-16 bg-background z-[100]">
+        <nav className="fixed w-full h-14 flex justify-center px-4 md:px-8 xl:px-16 bg-background z-[100] border-b-[1px] border-black">
             <div className="w-full h-full flex gap-8 xl:gap-16 items-center justify-between">
                 <div className='aspect-square bg-[#0000ff]'>
                     <AtSign color='transparent'/>
                 </div>
-                <div className="w-full flex justify-evenly md:justify-start gap-4 md:gap-8 xl:gap-16">
+                <div className="flex-1 flex justify-evenly gap-4 md:gap-8 xl:gap-16">
                     {NavbarRoutes.map((routes, i) => {
                         const isActive = current === routes.route[0];
                         return (
                             <a
                                 key={i}
                                 href={isActive ? "#" : routes.route[0]}
-                                className={`${isActive && 'underline text-[#0000ff]'} uppercase cursor-pointer hover:underline underline-offset-4 flex text-nowrap items-center text-base px-2 py-2`}
+                                className={`${isActive && 'underline text-[#0000ff]'} uppercase cursor-pointer hover:underline underline-offset-4 flex text-nowrap items-center text-base p-4`}
                             >
                                 <p
                                     ref={(el) => {
